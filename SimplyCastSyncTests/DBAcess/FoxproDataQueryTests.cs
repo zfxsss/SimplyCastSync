9f24d9a1ec099ae843d9b075c28f9bbe671ff35c
zfxsss <zhefeng_xu@hotmail.com>
2017-04-21 14:59:50 -0300

    update_20170421_3

--- SimplyCastSyncTests/DBAcess/FoxproDataQueryTests.cs
+++ SimplyCastSyncTests/DBAcess/FoxproDataQueryTests.cs
@@ -14,8 +14,11 @@ namespace SimplyCastSync.DBAccess.Tests
         [TestMethod()]
         public void GetDataTest()
         {
-            var foxquery = new FoxproDataQuery("Provider=VFPOLEDB.1;Data Source=C:\\Users\\zhefeng\\Documents\\Foxpro;");
-            var result = foxquery.GetData("select * from table_test1");
+            var foxquery = new FoxproDataQuery("Provider=VFPOLEDB.1;Data Source=C:\\v2k\\Data;");
+            var result = foxquery.GetData("select  a.ADDR1, a.ADDR2, a.ADDR3, a.EMAIL, a.HPHONE, a.MOBILE, a.POSTCODE, a.STATE, a.TITLE, a.OCCUPT, a.SURNAME, a.GIVEN_NAME, a.POSTADDR1, a.POSTADDR2, a.POSTADDR3, a.POSTSTATE, a.POSTPCODE, a.SEX, a.WPHONE, a.PHONE3, a.POSTNAME, a.BIRTHDAY, a.FIRSTVISIT, a.LASTRCLET, a.LASTRECALL, a.LASTVISIT, a.NEXTRCLET, a.NEXTRECALL, a.RECALL, a.RECALLSENT, a.FPOSTADDR, a.RCPERIOD, a.REFNUM, b.BOOKDATE, b.STARTTIME, b.STATUS, c.PAMOUNT from VPATIENT a left join (select k.BOOKDATE, k.STARTTIME, k.REFNUM, k.STATUS from (select max(BOOKDATE) as BOOKDATE, REFNUM from VAPPOINT group by REFNUM) h inner join VAPPOINT k on h.BOOKDATE = k.BOOKDATE and h.REFNUM = k.REFNUM) b on a.REFNUM = b.REFNUM left join (SELECT n.REFNUM, SUM(n.PAMOUNT) as PAMOUNT FROM (SELECT MAX(ACCDATE) as ACCDATE,REFNUM from VACCOUNT GROUP BY REFNUM) m inner join VACCOUNT n on m.ACCDATE = n.ACCDATE and m.REFNUM = n.REFNUM GROUP BY n.REFNUM) c on a.REFNUM = c.REFNUM where (!EMPTY(TRIM(a.SURNAME)) OR !EMPTY(TRIM(a.GIVEN_NAME))) order by a.REFNUM");
+
+            var oo =  result.Tables[0].Select("REFNUM = 1");
+
             Assert.Fail();
         }
     }
